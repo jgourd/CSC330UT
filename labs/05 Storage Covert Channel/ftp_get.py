b8d30ea49d2a0f672dfc0581e4c266ea0ed1805b
Jean Gourd <jgourd@ut.edu>
2025-03-07 10:22:12 -0500

    jgourd: updated the FTP sample program from lab05.

--- labs/05 Storage Covert Channel/ftp_get.py
+++ labs/05 Storage Covert Channel/ftp_get.py
@@ -12,6 +12,11 @@ USER = "anonymous"
 PASSWORD = ""
 FOLDER = ""
 USE_PASSIVE = True # set to False if the connection times out
+# different FTP file/folder (directory) listing methods:
+# method 1 uses ftp.dir which only shows visible items (i.e., items that aren't hidden -- which begin with '.' on Linux systems)
+# method 2 uses ftrp.retrlines which can optionally show hidden files (including the current and parent directories); however, this method requires more work to remove unnecessary items
+# I recommend using method 2 to look around an FTP server to help determine where relevant items might be located, then switch to method 1 to actually fetch the items for decoding the covert message
+LIST_METHOD = 1
 
 # connect and login to the FTP server
 ftp = FTP()
@@ -22,14 +27,20 @@ ftp.set_pasv(USE_PASSIVE)
 # navigate to the specified directory and list files
 ftp.cwd(FOLDER)
 files = []
-ftp.dir(files.append)
-
+# listing files method #1
+if (LIST_METHOD == 1):
+    ftp.dir(files.append)
+elif (LIST_METHOD == 2):
+    # use "LIST -a" to also include hidden items
+    ftp.retrlines("LIST", files.append)
 # exit the FTP server
 ftp.quit()
 
 # sort the files by filename, ignoring case
+# use this if the listing retrieved from the FTP server isn't ordered/sorted properly
 #files = sorted(files, key=lambda x: x[56:].lower())
 
 # display the folder contents
 for f in files:
-	print(f)
+    print(f)
+
